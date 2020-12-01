ed1db9aa47d065a1765f05470f530c7aaed155ae
Jeff <euleramon@gmail.com>
2020-12-01 23:37:02 +0800

    format code

--- app/src/main/java/com/codingwithmitch/todolist/MainActivity.java
+++ app/src/main/java/com/codingwithmitch/todolist/MainActivity.java
@@ -1,4 +1,5 @@
 package com.codingwithmitch.todolist;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
@@ -17,19 +18,19 @@ import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
+	private static final String TAG = "MainActivity";
 
-    //ui
-    private TextView text;
+	//ui
+	private TextView text;
 
-    // vars
-    private CompositeDisposable disposables = new CompositeDisposable();
+	// vars
+	private CompositeDisposable disposables = new CompositeDisposable();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        text = findViewById(R.id.text);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		text = findViewById(R.id.text);
 
 //        text.setText("doing stuff...");
 //        Observable.fromIterable(DataSource.createTasksList())
@@ -101,38 +102,38 @@ public class MainActivity extends AppCompatActivity {
 //                    }
 //                });
 
-        Observable<Task> taskObservable = Observable
-                .fromIterable(DataSource.createTasksList())
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
+		Observable<Task> taskObservable = Observable
+				.fromIterable(DataSource.createTasksList())
+				.subscribeOn(Schedulers.io())
+				.observeOn(AndroidSchedulers.mainThread());
 
-        taskObservable.subscribe(new Observer<Task>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe: called.");
-            }
+		taskObservable.subscribe(new Observer<Task>() {
+			@Override
+			public void onSubscribe(Disposable d) {
+				Log.d(TAG, "onSubscribe: called.");
+			}
 
-            @Override
-            public void onNext(Task task) {
-                Log.d(TAG, "onNext: " + Thread.currentThread().getName());
-                Log.d(TAG, "onNext: " + task.getDescription());
+			@Override
+			public void onNext(Task task) {
+				Log.d(TAG, "onNext: " + Thread.currentThread().getName());
+				Log.d(TAG, "onNext: " + task.getDescription());
 //                try {
 //                    Thread.sleep(1000);
 //                } catch (Exception e) {
 //                    e.printStackTrace();
 //                }
-            }
+			}
 
-            @Override
-            public void onError(Throwable e) {
-                Log.e(TAG, "onError: ", e);
-            }
+			@Override
+			public void onError(Throwable e) {
+				Log.e(TAG, "onError: ", e);
+			}
 
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "onComplete: called.");
-            }
-        });
+			@Override
+			public void onComplete() {
+				Log.d(TAG, "onComplete: called.");
+			}
+		});
 
 
 //        Observable.range(0,3)
@@ -160,14 +161,14 @@ public class MainActivity extends AppCompatActivity {
 //
 //                    }
 //                });
-    }
+	}
 
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
 //        disposables.clear();
-    }
+	}
 }
 
 
